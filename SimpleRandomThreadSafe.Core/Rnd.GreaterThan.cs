a764c437a86b9176a6bb85f6d91f1a6b8c3efa8b
iluvadev <76512587+iluvadev@users.noreply.github.com>
2020-12-27 16:13:27 +0100

    Changes in Project name & namespaces

---
+++ SimpleRandomThreadSafe.Core/Rnd.GreaterThan.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimpleRandomThreadSafe
+{
+    public static partial class Rnd
+    {
+        public static double GreaterThan(double min)
+            => Between(min, double.MaxValue);
+
+        public static int GreaterThan(int min)
+            => Between(min, int.MaxValue);
+
+        public static byte GreaterThan(byte min)
+            => Between(min, byte.MaxValue);
+
+        public static short GreaterThan(short min)
+             => Between(min, short.MaxValue);
+
+        public static long GreaterThan(long min)
+            => Between(min, long.MaxValue);
+
+        public static decimal GreaterThan(decimal min)
+            => Between(min, decimal.MaxValue);
+    }
+}
---
+++ SimpleRandomThreadSafe.Core/Rnd.Types.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace SimpleRandomThreadSafe
+{
+    public static partial class Rnd
+    {
+        public static double Double()
+            => RandomLocal.NextDouble();
+
+        public static int Int32()
+            => RandomLocal.Next();
+
+        public static byte Byte()
+            => Between(byte.MinValue, byte.MaxValue);
+
+        public static short Int16()
+            => Between(short.MinValue, short.MaxValue);
+
+        public static long Int64()
+            => Between(long.MinValue, long.MaxValue);
+
+        public static decimal Decimal()
+            => Between(decimal.MinValue, decimal.MaxValue);
+
+        public static bool Boolean()
+            => Double() < 0.5;
+    }
+}
